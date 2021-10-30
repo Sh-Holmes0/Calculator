c8f65b68991a17f8db2aa3a12715339eb6181b51
Sherloc Holmes <ahmedzein3651@gmail.com>
2021-10-30 19:42:04 +0200

    spagitty

--- script.js
+++ script.js
@@ -1,10 +1,50 @@
-const btn1 = document.querySelector("button")
+const btn1 = document.querySelectorAll("button")
 const screen = document.querySelector("h1")
 
-const addText = () =>{
+const addText0 = () =>{
+    screen.appendChild(document.createTextNode("C"));
+}
+const addText1 = () =>{
+    screen.appendChild(document.createTextNode("/"));
+}
+const addText2 = () =>{
+    screen.appendChild(document.createTextNode("*"));
+}
+const addText3 = () =>{
+    screen.appendChild(document.createTextNode("del"));
+}
+const addText4 = () =>{
     screen.appendChild(document.createTextNode("7"));
 }
+const addText5 = () =>{
+    screen.appendChild(document.createTextNode("8"));
+}
+const addText6 = () =>{
+    screen.appendChild(document.createTextNode("9"));
+}
+const addText7 = () =>{
+    screen.appendChild(document.createTextNode("-"));
+}
+const addText8 = () =>{
+    screen.appendChild(document.createTextNode("4"));
+}
+const addText9 = () =>{
+    screen.appendChild(document.createTextNode("5"));
+}
+const addText10 = () =>{
+    screen.appendChild(document.createTextNode("6"));
+}
+
+
+
 function testFun (){
     alert("hi its me");
 }
-btn1.addEventListener("click", addText)
+
+
+
+
+
+
+
+btn1[0].addEventListener("click", addText0) 
