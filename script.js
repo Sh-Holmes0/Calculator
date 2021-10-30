afbf87f32dd052c75bdd0c4e0afa83faef459a2b
Sherloc Holmes <ahmedzein3651@gmail.com>
2021-10-30 20:40:12 +0200

    spagitty overflow

--- script.js
+++ script.js
@@ -1,77 +1,79 @@
 const btn1 = document.querySelectorAll("button")
 const screen = document.querySelector("h1")
 
-const addText0 = () =>{
+
+const addText0 = () => {
     screen.appendChild(document.createTextNode("C"));
 }
-const addText1 = () =>{
-    screen.appendChild(document.createTextNode("/"));
+const addText1 = () => {
+    screen.appendChild(document.createTextNode(" / "));
 }
-const addText2 = () =>{
-    screen.appendChild(document.createTextNode("*"));
+const addText2 = () => {
+    screen.appendChild(document.createTextNode(" * "));
 }
-const addText3 = () =>{
+const addText3 = () => {
     screen.appendChild(document.createTextNode("del"));
 }
-const addText4 = () =>{
+const addText4 = () => {
     screen.appendChild(document.createTextNode("7"));
 }
-const addText5 = () =>{
+const addText5 = () => {
     screen.appendChild(document.createTextNode("8"));
 }
-const addText6 = () =>{
+const addText6 = () => {
     screen.appendChild(document.createTextNode("9"));
 }
-const addText7 = () =>{
-    screen.appendChild(document.createTextNode("-"));
+const addText7 = () => {
+    screen.appendChild(document.createTextNode(" - "));
 }
-const addText8 = () =>{
+const addText8 = () => {
     screen.appendChild(document.createTextNode("4"));
 }
-const addText9 = () =>{
+const addText9 = () => {
     screen.appendChild(document.createTextNode("5"));
 }
-const addText10 = () =>{
+const addText10 = () => {
     screen.appendChild(document.createTextNode("6"));
 }
-const addText11 = () =>{
-    screen.appendChild(document.createTextNode("+"));
+const addText11 = () => {
+    screen.appendChild(document.createTextNode(" + "));
 }
-const addText12 = () =>{
+const addText12 = () => {
     screen.appendChild(document.createTextNode("1"));
 }
-const addText13 = () =>{
+const addText13 = () => {
     screen.appendChild(document.createTextNode("2"));
 }
-const addText14 = () =>{
+const addText14 = () => {
     screen.appendChild(document.createTextNode("3"));
 }
-const addText15 = () =>{
-    screen.appendChild(document.createTextNode("."));
+const addText15 = () => {
+    screen.appendChild(document.createTextNode(" . "));
 }
-const addText16 = () =>{
-    screen.appendChild(document.createTextNode("("));
+const addText16 = () => {
+    screen.appendChild(document.createTextNode("( "));
 }
-const addText17 = () =>{
+const addText17 = () => {
     screen.appendChild(document.createTextNode("0"));
 }
-const addText18 = () =>{
-    screen.appendChild(document.createTextNode(")"));
+const addText18 = () => {
+    screen.appendChild(document.createTextNode(" )"));
 }
-function testFun (){
-    alert("hi its me");
+function testFun() {
+    // var h1Text = document.querySelector("h1").textContent;
+    alert(document.querySelector("h1").textContent);
 }
 
 
-btn1[0].addEventListener("click", testFun) 
-btn1[1].addEventListener("click", addText1) 
-btn1[2].addEventListener("click", addText2) 
-btn1[3].addEventListener("click", addText3) 
-btn1[4].addEventListener("click", addText4) 
-btn1[5].addEventListener("click", addText5) 
-btn1[6].addEventListener("click", addText6) 
-btn1[7].addEventListener("click", addText7) 
-btn1[8].addEventListener("click", addText8) 
+btn1[0].addEventListener("click", testFun)
+btn1[1].addEventListener("click", addText1)
+btn1[2].addEventListener("click", addText2)
+btn1[3].addEventListener("click", addText3)
+btn1[4].addEventListener("click", addText4)
+btn1[5].addEventListener("click", addText5)
+btn1[6].addEventListener("click", addText6)
+btn1[7].addEventListener("click", addText7)
+btn1[8].addEventListener("click", addText8)
 btn1[9].addEventListener("click", addText9)
 btn1[10].addEventListener("click", addText10)
 btn1[11].addEventListener("click", addText11)
